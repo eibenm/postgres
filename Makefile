5859c0615b667dff7186181d13e49d7d706ad448
Matt Eiben <eibenm@gmail.com>
2020-11-04 20:21:46 -0700

    Cleaning up makefile and update script to be ubuntu friendly

--- Makefile
+++ Makefile
@@ -5,11 +5,8 @@
 help:
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
-IP_ADDRESS="$(ifconfig en0 | grep inet | grep -v inet6 | awk '{print $2}')"
-TESTS='asshat'
-
 .PHONY: up
-up: ## Run the application.  ( assumes "make init" has already been run )
+up: ## Run the application.
 	@docker-compose up -d
 	@sh ./scripts/pgadmin.sh
 
--- scripts/pgadmin.sh
+++ scripts/pgadmin.sh
@@ -1,27 +1,20 @@
 #!/usr/bin/env bash
 
-GREEN='\033[1;32m'
-YELLOW='\033[1;33m'
-RESET='\033[0m'
-
-function successcolorecho() {
-    echo $GREEN"$@"$RESET  
-}
-
-function alertcolorecho() {
-    echo $YELLOW"$@"$RESET  
-}
-
 USERNAME="user@domain.com"
 PASSWORD="password"
-IP=$(ifconfig en0 | grep inet | grep -v inet6 | awk '{print $2}')
 
-successcolorecho "Pgadmin running at http://localhost:5000"
-successcolorecho "Pgadmin credentials:"
-alertcolorecho "\tUsername: $USERNAME"
-alertcolorecho "\tPassword: $PASSWORD"
-successcolorecho "Server connection info:"
-alertcolorecho "\tHost addess: $IP"
-successcolorecho "Database Credentials:"
-alertcolorecho "\tUsername: postgres"
-alertcolorecho "\tPassword: secret"
+if [ lsb_release 2>/dev/null ]; then
+    IP=$(ifconfig eth0 | grep 'inet ' | awk '{print $2}')
+else
+    IP=$(ifconfig en0 | grep inet | grep -v inet6 | awk '{print $2}')
+fi
+
+echo "Pgadmin running at http://localhost:5000"
+echo "Pgadmin credentials:"
+echo -e "\tUsername: $USERNAME"
+echo -e "\tPassword: $PASSWORD"
+echo "Server connection info:"
+echo -e "\tHost addess: $IP"
+echo "Database Credentials:"
+echo -e "\tUsername: postgres"
+echo -e "\tPassword: secret"
